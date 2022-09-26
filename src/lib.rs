1110cf914f3a1def48aa5581fc21061abcc315af
Mathijs de Boer <czorio4@gmail.com>
2022-09-27 00:22:31 +0200

    Fix naming issues causing builds to fail
    Signed-off-by: Mathijs de Boer <czorio4@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn fast_transform(_py: Python, m: &PyModule) -> PyResult<()> {
+fn fasttransform(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
